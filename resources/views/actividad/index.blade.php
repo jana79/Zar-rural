f73c160a863e3a0dbc076ec55b01dc6bd9a2d24f
Jana <jana.modrego@flat101.es>
2020-06-06 07:35:30 +0200

    Correción de vista con imagen que faltaba

--- resources/views/actividad/index.blade.php
+++ resources/views/actividad/index.blade.php
@@ -28,36 +28,36 @@
 <h1>Bienvenido a la sección de Actividades</h1>
 <p class="text-center">Encuentra tu próximo plan.</p>
 <div class="row container mx-auto pt-3">
-    <div class="col-12 col-md-6">  
+    <div class="col-12 col-md-6">
         <h3 class="text-center verde py-4">Naturaleza</h3>
-        <a href="{{url('actividad/naturaleza')}}"> 
-            <img class=" img-fluid" src="{{asset('/images/cascada.jpg')}}" 
+        <a href="{{url('actividad/naturaleza')}}">
+            <img class=" img-fluid" src="{{asset('/images/cascada.jpg')}}"
                  alt="Patrimonio">
-        </a>      
+        </a>
     </div>
-    <div class="col-12 col-md-6"> 
+    <div class="col-12 col-md-6">
         <h3 class="text-center verde py-4">Patrimonio</h3>
-        <a href="{{url('actividad/patrimonio')}}"> 
-            <img class=" img-fluid" src="{{asset('/images/colegiata.jpg')}}" 
+        <a href="{{url('actividad/patrimonio')}}">
+            <img class=" img-fluid" src="{{asset('/images/colegiata.jpg')}}"
                  alt="Patrimonio">
-        </a>      
+        </a>
     </div>
 </div>
 
 <div class="row container mx-auto pt-3 pb-5">
-    <div class="col-12 col-md-6">  
+    <div class="col-12 col-md-6">
         <h3 class="text-center verde py-4">Ocio</h3>
-        <a href="{{url('actividad/ocio')}}"> 
-            <img class=" img-fluid" src="{{asset('/images/museoVino.jpg')}}" 
+        <a href="{{url('actividad/ocio')}}">
+            <img class=" img-fluid" src="{{asset('/images/museoVino.jpg')}}"
                  alt="Ocio">
-        </a>        
+        </a>
     </div>
-    <div class="col-12 col-md-6"> 
+    <div class="col-12 col-md-6">
         <h3 class="text-center verde py-4">Tradición</h3>
-        <a href="{{url('actividad/tradicion')}}"> 
-            <img class=" img-fluid" src="{{asset('/images/contradanza.jpg')}}" 
+        <a href="{{url('actividad/tradicion')}}">
+            <img class=" img-fluid" src="{{asset('/images/cipotegato.jpg')}}"
                  alt="Tradición">
-        </a>      
+        </a>
     </div>
 </div>
 
--- resources/views/actividad/index_col.blade.php
+++ resources/views/actividad/index_col.blade.php
@@ -29,41 +29,41 @@
 </div>
 
 <div class="row container mx-auto pt-3">
-    <div class="col-12 col-md-6">  
+    <div class="col-12 col-md-6">
         <h3 class="text-center verde py-4">Naturaleza</h3>
-        <a href="#"> 
-            <img class=" img-fluid" src="{{asset('/images/cascada.jpg')}}" 
+        <a href="#">
+            <img class=" img-fluid" src="{{asset('/images/cascada.jpg')}}"
                  alt="Naturaleza">
-        </a>      
+        </a>
     </div>
-    <div class="col-12 col-md-6"> 
+    <div class="col-12 col-md-6">
         <h3 class="text-center verde py-4">Patrimonio</h3>
-        <a href="{{url('actividades/patrimonio')}}"> 
-            <img class=" img-fluid" src="{{asset('/images/colegiata.jpg')}}" 
+        <a href="{{url('actividades/patrimonio')}}">
+            <img class=" img-fluid" src="{{asset('/images/colegiata.jpg')}}"
                  alt="Patrimonio">
-        </a>      
+        </a>
     </div>
 </div>
 
 <div class="row container mx-auto pt-3 pb-5">
-    <div class="col-12 col-md-6">  
+    <div class="col-12 col-md-6">
         <h3 class="text-center verde py-4">Ocio</h3>
-        <a href="#"> 
-            <img class=" img-fluid" src="{{asset('/images/museoVino.jpg')}}" 
+        <a href="#">
+            <img class=" img-fluid" src="{{asset('/images/museoVino.jpg')}}"
                  alt="Ocio">
-        </a>        
+        </a>
     </div>
-    <div class="col-12 col-md-6"> 
+    <div class="col-12 col-md-6">
         <h3 class="text-center verde py-4">Tradición</h3>
-        <a href="#"> 
-            <img class=" img-fluid" src="{{asset('/images/alfonsadas.jpg')}}" 
+        <a href="#">
+            <img class=" img-fluid" src="{{asset('/images/cipotegato.jpg')}}"
                  alt="Tradición">
-        </a>      
+        </a>
     </div>
 </div>
 <div class="col-12 d-flex justify-content-center pt-5">
     <div>
-        <a href="{{route("actividad.create")}}" 
+        <a href="{{route("actividad.create")}}"
            class="btn btn-verde">Añadir actividad</a>
     </div>
 </div>
